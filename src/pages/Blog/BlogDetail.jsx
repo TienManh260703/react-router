22904e8d4e092dc14e607cf5ef221af86c738d90
manh_03 <nguyenmanh19924@gmail.com>
2024-06-07 01:40:39 +0700

    Hooks React Router : useParams()

--- src/pages/Blog/BlogDetail.jsx
+++ src/pages/Blog/BlogDetail.jsx
@@ -1,7 +1,46 @@
-function BlogDetail (){
-    return(
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+function BlogDetail() {
+
+    const param = useParams();
+
+    const [post, setPost] = useState();
+
+    // console.log("Param id : " , param);
+
+    useEffect(() => {
+        fetch(`https://dummyjson.com/posts/${param.id}`)
+            .then(res => res.json())
+            .then(data => {
+                setPost(data)
+            })
+    }, []);
+
+    console.log(post);
+
+    return (
         <>
-        Page Blog Detail
+            Page Blog Detail
+            {post && (
+
+                <>
+                    <h1>{post.title}</h1>
+                    <div>
+                        {post.body} <br />
+                        Like : {post.reactions.likes} <br />
+                        Dislikes : {post.reactions.dislikes} <br />
+                        Views : {post.views} <br />
+                        Tag :
+                        {post.tags.map((item, index) => (
+                            <span key={index}>
+                                <Link>{item}</Link>
+                            </span>
+                        ))}
+                    </div>
+                </>
+            )}
         </>
     )
 }
