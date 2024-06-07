21714de354d0dd2ff18401ad36b4f76e28052b2e
manh_03 <nguyenmanh19924@gmail.com>
2024-06-07 10:13:43 +0700

    Update folder layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,18 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Error404 from './pages/Error404'
-import LayoutDefault from './assets/layout/LayoutDefault/LayoutDefault'
 import Blog from './pages/Blog/Blog'
 import BlogNew from './pages/Blog/BlogNew'
 import BlogRelated from './pages/Blog/BlogRelated'
 import BlogAll from './pages/Blog/BlogAll'
 import BlogDetail from './pages/Blog/BlogDetail'
-
+import LayoutDefault from './layout/LayoutDefault/LayoutDefault'
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<LayoutDefault />}>  {/* Sử dụng outlet để hiển thị nội dụng => dùng trong LayOutDefault  */}
+        <Route path="/" element={<LayoutDefault/>}>  {/* Sử dụng outlet để hiển thị nội dụng => dùng trong LayOutDefault  */}
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
